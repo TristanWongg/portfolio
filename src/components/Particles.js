3a2bf4c1bf6662563b744c85cd7732729a44cbc2
Tristan Wong <tristanwong321@hotmail.com>
2023-03-20 16:44:04 -0400

    updated particles component comments

--- src/components/Particles.js
+++ src/components/Particles.js
@@ -4,10 +4,8 @@ import { loadSlim } from "tsparticles-slim"; // loads tsparticles-slim
 import { useCallback, useMemo } from "react";
 
 const ParticlesComponent = (props) => {
-  // using useMemo is not mandatory, but it's recommended since this value can be memoized if static
+  
   const options = useMemo(() => {
-    // using an empty options object will load the default options, which are static particles with no background and 3px radius, opacity 100%, white color
-    // all options can be found here: https://particles.js.org/docs/interfaces/Options_Interfaces_IOptions.IOptions.html
     return {
       fullScreen: {
         enable: true, // enabling this will make the canvas fill the entire screen, it's enabled by default
@@ -58,7 +56,6 @@ const ParticlesComponent = (props) => {
     };
   }, []);
 
-  // useCallback is not mandatory, but it's recommended since this callback can be memoized if static
   const particlesInit = useCallback((engine) => {
     loadSlim(engine);
     // loadFull(engine); // for this sample the slim version is enough, choose whatever you prefer, slim is smaller in size but doesn't have all the plugins and the mouse trail feature
